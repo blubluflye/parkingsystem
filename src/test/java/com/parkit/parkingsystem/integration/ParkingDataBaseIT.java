9a3588df9179ea3e650c794fdbb73d7723b102b5
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2022-07-11 18:52:11 +0200

    the two test TODO have been completed test database: ticket saved and price and parkingslotavaibility

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,13 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -15,6 +19,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
 
+import java.util.Date;
+
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
 
@@ -22,6 +28,8 @@ public class ParkingDataBaseIT {
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
+    private static FareCalculatorService fareCalculatorService;
+    private Ticket ticket;
 
     @Mock
     private static InputReaderUtil inputReaderUtil;
@@ -33,6 +41,7 @@ public class ParkingDataBaseIT {
         ticketDAO = new TicketDAO();
         ticketDAO.dataBaseConfig = dataBaseTestConfig;
         dataBasePrepareService = new DataBasePrepareService();
+        fareCalculatorService = new FareCalculatorService();
     }
 
     @BeforeEach
@@ -40,6 +49,7 @@ public class ParkingDataBaseIT {
         when(inputReaderUtil.readSelection()).thenReturn(1);
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         dataBasePrepareService.clearDataBaseEntries();
+        ticket = new Ticket();
     }
 
     @AfterAll
@@ -51,7 +61,29 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        // check if a ticket that is saved in DB and Parking table is updated with availability
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);       
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  63 * 60 * 1000) );
+        Date outTime = new Date();
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        ticket.setParkingSpot(parkingSpot);
+        ticket.setVehicleRegNumber("ABCDEFG");
+        if(ticketDAO.saveTicket(ticket)) {
+        	assert(!parkingSpot.isAvailable());
+        }
+        else
+        {
+        	//get ticket from DB
+        	Ticket ticketFromDB = ticketDAO.getTicket("ABCDEFG");
+        	//reduce the range from milliseconds to min because DB round them to second then compare the two ticket
+        	assert(ticket.getInTime().getTime()/60000 == ticketFromDB.getInTime().getTime()/60000);
+        	// use getNextAvailableSlot from parkingSpotDAO to check if parkingSpot has been updated in DB
+        	assert(parkingSpotDAO.getNextAvailableSlot(parkingSpot.getParkingType()) > parkingSpot.getId());
+        }
     }
 
     @Test
@@ -60,6 +92,11 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+        Ticket ticket = ticketDAO.getTicket("ABCDEFG");
+        double priceFromDB = ticket.getPrice();
+        FareCalculatorService fareCalculatorService = new FareCalculatorService();
+        fareCalculatorService.calculateFare(ticket);
+        assert(ticket.getPrice() == priceFromDB);
     }
 
 }
